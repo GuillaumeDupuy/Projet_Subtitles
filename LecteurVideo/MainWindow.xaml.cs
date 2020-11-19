cc3571fbc37886267fa7fb9acce677b2dcfe1d54
Guillaume <56391911+GuillaumeDupuy@users.noreply.github.com>
2020-11-19 17:28:54 +0100

    fix WPF

--- LecteurVideo/MainWindow.xaml.cs
+++ LecteurVideo/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Projet_Subtitle
 {
@@ -42,7 +31,7 @@ namespace Projet_Subtitle
             {
                 mediaElement.Source = new Uri(boite.FileName);
                 T = textBlock;
-                test = new Program(T,cheminVideo);
+                test = new Program(T, cheminVideo);
             }
         }
 
